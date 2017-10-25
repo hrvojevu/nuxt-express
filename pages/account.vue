7e82dc2d608c7dea07a9d1fba8224100b03c5e2e
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2017-10-25 14:02:03 +0200

    Setting up accounts page

---
+++ pages/account.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    Account
+  </section>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Account'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
