e9409e8c6616c07f62f270aff1b3e43a46008ae4
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2017-10-26 14:58:04 +0200

    Updating axios baseURL

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios'
 
 export default axios.create({
-  baseURL: '/api',
+  baseURL: `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}/api`,
   withCredentials: true,
   headers: {
     'content-type': 'application/json'
