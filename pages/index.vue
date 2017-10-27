6eef19a083eaf137f66b711fecd804eabc2d1ecf
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2017-10-27 15:15:16 +0200

    Index page update

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,22 @@
 <template>
   <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <p v-if="users.length === 0">There are no users</p>
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: user.id }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
-    <v-btn color="primary">Add</v-btn>
+    <h1 class="title">Home</h1>
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
-import { mapGetters } from 'vuex'
-
 export default {
-  async asyncData () {
-    let { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
-    // let { data } = await axios.get('/api/users')
-    return { users: data }
-  },
-  head () {
-    return {
-      title: 'Users'
-    }
-  },
-  computed: {
-    ...mapGetters({
-      user: 'auth/user',
-      isAdmin: 'auth/isAdmin'
-    })
-  },
-  methods: {
-  }
 }
 </script>
 
 <style scoped lang="scss">
-.title
-{
-  margin: 30px 0;
-}
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
+@import '../assets/scss/variables';
+
+.container {
+  padding-bottom: $footer-offset;
+
+  .title {
+    margin: 30px 0;
+  }
 }
 </style>
