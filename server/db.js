eb3beb9fa2f16476c86fa77d61c22c2c6ae76585
Hrvoje Vucic <hrvoje.vucic@hotmail.com>
2017-11-11 11:12:38 +0100

    Updating DB connection string

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const Sequelize = require('sequelize')
 
-const host = process.env.DB_HOST || '127.0.0.1'
-const dbName = process.env.DB_NAME || 'nuxt-express'
-const user = process.env.DB_USER || 'root'
-const pwd = process.env.DB_USER_PWD || 'admin'
+const connectionString = process.env.CLEARDB_DATABASE_URL || 'mysql://root:admin@127.0.0.1/nuxt-express'
 
-const sequelize = new Sequelize(`mysql://${user}:${pwd}@${host}/${dbName}`, {
+const sequelize = new Sequelize(connectionString, {
   logging: true
 })
 
